7efb1dc1ccf2477734a220a5c2619fe8f58b983a
Fatih Böken <bokenfatih@gmail.com>
2024-04-02 14:46:15 +0300

    start

---
+++ src/pages/Home.jsx
@@ -0,0 +1,13 @@
+import Doctors from "../components/Doctors";
+
+const Home = () => {
+  return (
+    <main className="text-center mt-2">
+      <h1 className="display-5 text-danger">CLARUS HOSPITAL</h1>
+      <Doctors/>
+      {/* <AppointmentList apps={appointments} setApps={setAppointments} /> */}
+    </main>
+  );
+};
+
+export default Home
